e8a345303ceca3363ffa21d3db78095add927d4f
benjaminbills <benjaminoba234@gmail.com>
2022-01-23 19:57:56 +0300

    setup api

--- address/views.py
+++ address/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Address
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import AddressSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def addressList(request):
+  address =Address.objects.all()
+  serializer = AddressSerializer(address, many=True)
+  return Response(serializer.data)
