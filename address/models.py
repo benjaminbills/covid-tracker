6cfa8934593cba0c0ddcd3b56ba9339ddb9b307a
benjaminbills <benjaminoba234@gmail.com>
2022-01-23 19:51:47 +0300

    added address model

--- address/models.py
+++ address/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+class Address(models.Model):
+  name = models.CharField(max_length=50, blank=True, null=True)
+  address = models.CharField(max_length=500)
+  lat = models.FloatField(blank=True, null=True)
+  long = models.FloatField(blank=True, null=True)
+  date = models.DateField(blank=True, null=True)
